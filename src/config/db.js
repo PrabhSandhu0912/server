542f0cac7e40a1669bba153d3270e72330146a34
PrabhSandhu0912 <147322966+PrabhSandhu0912@users.noreply.github.com>
2023-12-21 17:52:33 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const mongoDbUrl="mongodb://mongo:2hB2bG3gBBGcBf23e161GGDae1f1b4E6@monorail.proxy.rlwy.net:12754"
+const mongoDbUrl="mongodb+srv://clickitserver:yXkvXYlVJI5n9JbQ@serverclickit.pasm1v7.mongodb.net/?retryWrites=true&w=majority"
 const connectDb=()=>{
     return mongoose.connect(mongoDbUrl)
 }
