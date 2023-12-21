f29c584f8d2ed3001e28f9f1b71c7cc9ce928f99
PrabhSandhu0912 <147322966+PrabhSandhu0912@users.noreply.github.com>
2023-12-21 16:23:25 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const mongoDbUrl="mongodb://mongo:9tw3FIuAJl02ozOUrfmg@containers-us-west-190.railway.app:7401"
+const mongoDbUrl="mongodb://mongo:2hB2bG3gBBGcBf23e161GGDae1f1b4E6@monorail.proxy.rlwy.net:12754"
 const connectDb=()=>{
     return mongoose.connect(mongoDbUrl)
 }
