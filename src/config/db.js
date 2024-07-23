f242a50e8fe1acf268d8b2181e59cd1379c4b9e7
PrabhSandhu0912 <147322966+PrabhSandhu0912@users.noreply.github.com>
2024-07-23 09:37:02 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose")
 
-const mongoDbUrl="mongodb+srv://sandhuprabh342:iocQuBh3O828IB5k@cluster0.gcpnusw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
-"
+const mongoDbUrl="mongodb+srv://sandhuprabh342:Sandhu0912@justdial.4bhf7jr.mongodb.net/?retryWrites=true&w=majority&appName=JustDial"
 const connectDb=()=>{
     return mongoose.connect(mongoDbUrl)
 }
